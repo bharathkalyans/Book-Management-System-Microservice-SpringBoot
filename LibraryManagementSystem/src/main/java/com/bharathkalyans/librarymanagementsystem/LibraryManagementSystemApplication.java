23dd5673ba9f0d6d82f0d2eb4fdac07d2b980711
Bharath Kalyan S <sbharath35@gmail.com>
2022-12-15 12:39:39 +0530

    Add files via upload

---
+++ LibraryManagementSystem/src/main/java/com/bharathkalyans/librarymanagementsystem/LibraryManagementSystemApplication.java
@@ -0,0 +1,19 @@
+package com.bharathkalyans.librarymanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class LibraryManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementSystemApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplete() {
+        return new RestTemplate();
+    }
+}
